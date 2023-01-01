3349c6a60186622f3ed4a4bd450fcb42045f1290
nadia-newaz <nadia.nitu55@gmail.com>
2023-01-01 16:10:29 +0600

    js project

---
+++ index.js
@@ -0,0 +1,9 @@
+let array = [50,40,61,87];
+
+let addition = 0;
+for(let i = 0; i < array.length; i++){
+    let result = array[i];
+    addition = addition + result;
+   console.log(addition);
+   
+}
